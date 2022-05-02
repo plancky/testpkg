c2801d74cdcf56cec06c73377f04d30df9132404
plancky <80216472+plancky@users.noreply.github.com>
2022-05-02 10:08:05 +0530

    t2

--- testpkg.py
+++ testpkg.py
@@ -4,6 +4,6 @@ names = ['matplotlib','numpy','sympy','scipy','pandas']
 for name in names:
     try:
         exec("import "+name)
-        print('{name}',"found")
+        print('{0}'.format(name),"found")
     except:
         print("{0} NOT found".format(name)) 
